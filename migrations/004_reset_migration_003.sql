b40eba03388564c56e58cb2e1e0655801ee682c4
Aliaksei <stepun@gmail.com>
2025-10-01 22:57:05 +0300

    Remove migration 004 - no longer needed

--- migrations/004_reset_migration_003.sql
+++
@@ -1,6 +0,0 @@
--- Migration 004: Reset migration 003 to allow re-application
--- Date: 2025-10-01
--- Task: Remove failed migration 003 record to allow it to run again with fixed transaction logic
-
--- Remove migration 003 from schema_migrations so it can be re-applied
-DELETE FROM schema_migrations WHERE version = '003_remove_premium_fields.sql';
